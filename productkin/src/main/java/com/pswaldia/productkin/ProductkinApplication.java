84066e11a1bb45c62e6cbda1a5e5e0d6bc4ae50b
Pradeep Singh <pradeepsingh5@deloitte.com>
2021-09-12 09:05:08 +0530

    initialized spring project

---
+++ productkin/src/main/java/com/pswaldia/productkin/ProductkinApplication.java
@@ -0,0 +1,13 @@
+package com.pswaldia.productkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductkinApplication.class, args);
+	}
+
+}
---
+++ productkin/src/test/java/com/pswaldia/productkin/ProductkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pswaldia.productkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
